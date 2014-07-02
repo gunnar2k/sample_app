56012aff2227507e8d146d677c694b4088c99408
Gunnar Fornes <gunnarfornes@gmail.com>
2014-07-02 22:37:59 +0200

    Added unit testing, postgresql locally, guard, spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+	let(:title) {"Ruby on Rails Tutorial Sample App"}
+
 	describe "Home page" do
+
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end 
 
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title(title)
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
 		end
 	end
 
 	describe "Help page" do
+		
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
@@ -21,11 +31,12 @@ describe "Static pages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{title} | Help")
 		end
 	end
 
 	describe "About page" do
+
 		it "should have the content 'About'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About')
@@ -33,7 +44,7 @@ describe "Static pages" do
 
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+			expect(page).to have_title("#{title} | About")
 		end
 	end
 
